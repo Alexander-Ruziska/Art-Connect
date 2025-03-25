1e14bd7c77ea0f00eac0c0b9f98d84224e865fa2
Alexander Ruziska <ziskapants@gmail.com>
2025-03-25 10:45:54 -0500

    adjusted code so get routes can be seen by public

--- server/routes/artists.router.js
+++ server/routes/artists.router.js
@@ -3,39 +3,40 @@ const router = express.Router();
 const pool = require('../modules/pool');
 const {rejectUnauthenticated, rejectIfNotArtist} = require('../modules/authentication-middleware');
 
-
 // GET all artists
-router.get('/', rejectUnauthenticated, async (req, res) => {
-  const queryText = 'SELECT * FROM "artists";'; 
-  try {
-    const result = await pool.query(queryText); 
-    res.send(result.rows);
-  } catch (err) {
-    console.error('Error in GET /api/artists:', err);
-    res.sendStatus(500);
-  }
+// router.get('/', rejectUnauthenticated, async (req, res) => {
+//   const queryText = 'SELECT * FROM "artists";'; 
+//   try {
+//     const result = await pool.query(queryText); 
+//     res.send(result.rows);
+//   } catch (err) {
+//     console.error('Error in GET /api/artists:', err);
+//     res.sendStatus(500);
+//   }
+// });
+
+router.get('/', (req, res) => {
+  pool.query('SELECT * FROM "artists"')
+    .then((result) => res.send(result.rows))
+    .catch((err) => {
+      console.error('GET /api/artists error:', err);
+      res.sendStatus(500);
+    });
 });
+
 // GET ARTIST BY ID
-router.get('/:id', rejectUnauthenticated,  async (req, res) => {
-  const artistId = req.params.id;
-  const queryText = 'SELECT * FROM "artists" WHERE "id" = $1;';
-  
-  try {
-    const result = await pool.query(queryText, [artistId]);
 
-    if (result.rows.length === 0) {
-      return res.status(404).send({ message: 'Artist not found' });
-    }
-
-    res.send(result.rows[0]);
-  } catch (err) {
-    console.error('Error in GET /api/artists/:id', err);
-    res.sendStatus(500);
-  }
+router.get('/:id', (req, res) => {
+  const sqlText = 'SELECT * FROM "artists" WHERE id = $1';
+  pool.query(sqlText, [req.params.id])
+    .then((result) => res.send(result.rows[0]))
+    .catch((err) => {
+      console.error('GET /api/artists/:id error:', err);
+      res.sendStatus(500);
+    });
 });
 
-
-router.put('/update/:id', rejectUnauthenticated, async (req, res) => {
+router.put('/:id', rejectUnauthenticated, async (req, res) => {
   const artistId = req.params.id;        // 🟢 comes from URL like /update/5
   const userId = req.user.id;            // 🟢 comes from logged-in session
 
