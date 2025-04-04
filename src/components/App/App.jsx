4e7818ee1c02cac99f5090e8fd33f3d08f9dad14
Alexander Ruziska <ziskapants@gmail.com>
2025-04-04 15:30:29 -0500

    scolling fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,7 +3,8 @@ import Container from 'react-bootstrap/Container';
 import {
   Routes,
   Route,
-  Navigate
+  Navigate,
+  useLocation 
 } from "react-router-dom";
 
 import useStore from '../../zustand/store';
@@ -24,8 +25,9 @@ import AdminPage from '../AdminPage/AdminPage';
 import ArtistJobRequests from '../ArtistJobRequests';
 import OrgJobRequests from '../OrgJobRequests';
 
-
 function App() {
+  const location = useLocation(); 
+
   const user = useStore((state) => state.user);
   const fetchUser = useStore((state) => state.fetchUser);
   const fetchArtists = useStore((state) => state.fetchArtists);
@@ -37,187 +39,105 @@ function App() {
     fetchJobs();
   }, [fetchUser, fetchArtists, fetchJobs]);
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <>
-     <Container>
-      <header>
-        <Nav />
-      </header>
-      <main>
-        <Routes>
-          <Route 
-            exact path="/"
-            element={
-              user.id ? (
-                <HomePage />
-              ) : (
-                <HomePage />
-              )
-            }
-          />
-          <Route 
-            exact path="/login"
-            element={
-              user.id ? (
-                <Navigate to="/" replace />
-              ) : (
-                <LoginPage />
-              )
-            }
-          />
-          <Route 
-            exact path="/registration"
-            element={
-              user.id ? (
-                <Navigate to="/" replace />
-              ) : (
-                <RegisterPage />
-              )
-            }
-          />
-          <Route 
-            exact path="/admin"
-            element={
-              user?.is_admin ? (
-                <AdminPage />
-              ) : (
-                <Navigate to="/" replace />
-              )
-            }
-          />
-
-          <Route 
-            exact path="/artists"
-            element={
-              user.id ? (
-                <ArtistList />
-              ) : (
-                <ArtistList />
-              )
-            }
-          />
-          <Route 
-            exact path="/job-list"
-            element={
-              user.id ? (
-                <JobList />
-              ) : (
-                <LoginPage/>
-              )
-            }
-          />
-          <Route 
-            exact path="/ideas"
-            element={
-              user.artist_id ? (
-                <IdeaForm />
-              ) : (
-                <RegisterPage />
-              )
-            }
-          />
-          <Route 
-            exact path="/organization-list/:organizationId"
-            element={
-              user.id ? (
-                <OrganizationPage />
-              ) : (
-                <OrganizationPage />
-              )
-            }
-          />
-          <Route 
-            exact path="/organization-list"
-            element={
-              user.id ? (
-                <OrganizationList />
-              ) : (
-                <OrganizationList />
-              )
-            }
-          />
-          <Route 
-            exact path="/post-job/:id"
-            element={
-              user?.is_organization ? (
-                <PostJob />
-              ) : (
-                <p>You must be an organization to post a job.</p>
-              )
-            }
-          />
-
-          <Route 
-            exact path="/my-job-requests"
-            element={
-              user.artist_id ? (
-                <ArtistJobRequests />
-              ) : (
-                <Navigate to="/login" />
-              )
-            }
-          />     
-
-          <Route 
-            exact path="/job-requests"
-            element={
-              user.organization_id ? (
-                <OrgJobRequests />
-              ) : (
-                <Navigate to="/login" />
-              )
-            }
-          />
-
-          <Route path='/artists/:artistId' element={<ArtistPage />} />
-          <Route path='/artists/:artistId/ideas' element={<ArtistIdea />} />
-          <Route path='/photos' element={<EditMedia />} />
-
-          <Route 
-            exact path="/about"
-            element={
-              <>
-                <h2>About Page</h2>
-                <p>
-                  Intelligence doesn’t seem like an aspect of personal character, and it isn’t.
-                  Coincidentally, great intelligence is only loosely connected to being a good programmer.
-                </p>
-                <p>
-                  What? You don’t have to be superintelligent?
-                </p>
-                <p>
-                  No, you don’t. Nobody is really smart enough to program computers.
-                  Fully understanding an average program requires an almost limitless capacity
-                  to absorb details and an equal capacity to comprehend them all at the same time.
-                  The way you focus your intelligence is more important than how much intelligence you have…
-                </p>
-                <p>
-                  …most of programming is an attempt to compensate for the strictly limited size of our skulls.
-                  The people who are the best programmers are the people who realize how small their brains are.
-                  They are humble. The people who are the worst at programming are the people who refuse to
-                  accept the fact that their brains aren’t equal to the task.
-                  Their egos keep them from being great programmers.
-                  The more you learn to compensate for your small brain, the better a programmer you’ll be.
-                  <span className="squiggle"> The more humble you are, the faster you’ll improve.</span>
-                </p>
-                <p>
-                  --From Steve McConnell's <em>Code Complete</em>.
-                </p>
-              </>
-            }
-          />
-          <Route
-            path="*"
-            element={
-              <h2>404 Page</h2>
-            } 
-          />
-        </Routes>
-      </main>
-      <footer>
-        <p>Copyright © {new Date().getFullYear()}</p>
-      </footer>
-     </Container>
+      <Container>
+        <header>
+          <Nav />
+        </header>
+        <main>
+          <Routes>
+            <Route 
+              exact path="/"
+              element={user.id ? <HomePage /> : <HomePage />}
+            />
+            <Route 
+              exact path="/login"
+              element={user.id ? <Navigate to="/" replace /> : <LoginPage />}
+            />
+            <Route 
+              exact path="/registration"
+              element={user.id ? <Navigate to="/" replace /> : <RegisterPage />}
+            />
+            <Route 
+              exact path="/admin"
+              element={user?.is_admin ? <AdminPage /> : <Navigate to="/" replace />}
+            />
+            <Route 
+              exact path="/artists"
+              element={user.id ? <ArtistList /> : <ArtistList />}
+            />
+            <Route 
+              exact path="/job-list"
+              element={user.id ? <JobList /> : <LoginPage />}
+            />
+            <Route 
+              exact path="/ideas"
+              element={user.artist_id ? <IdeaForm /> : <RegisterPage />}
+            />
+            <Route 
+              exact path="/organization-list/:organizationId"
+              element={user.id ? <OrganizationPage /> : <OrganizationPage />}
+            />
+            <Route 
+              exact path="/organization-list"
+              element={user.id ? <OrganizationList /> : <OrganizationList />}
+            />
+            <Route 
+              exact path="/post-job/:id"
+              element={user?.is_organization ? <PostJob /> : <p>You must be an organization to post a job.</p>}
+            />
+            <Route 
+              exact path="/my-job-requests"
+              element={user.artist_id ? <ArtistJobRequests /> : <Navigate to="/login" />}
+            />
+            <Route 
+              exact path="/job-requests"
+              element={user.organization_id ? <OrgJobRequests /> : <Navigate to="/login" />}
+            />
+            <Route path='/artists/:artistId' element={<ArtistPage />} />
+            <Route path='/artists/:artistId/ideas' element={<ArtistIdea />} />
+            <Route path='/photos' element={<EditMedia />} />
+            <Route 
+              exact path="/about"
+              element={
+                <>
+                  <h2>About Page</h2>
+                  <p>
+                    Intelligence doesn’t seem like an aspect of personal character, and it isn’t.
+                    Coincidentally, great intelligence is only loosely connected to being a good programmer.
+                  </p>
+                  <p>What? You don’t have to be superintelligent?</p>
+                  <p>
+                    No, you don’t. Nobody is really smart enough to program computers.
+                    Fully understanding an average program requires an almost limitless capacity
+                    to absorb details and an equal capacity to comprehend them all at the same time.
+                    The way you focus your intelligence is more important than how much intelligence you have…
+                  </p>
+                  <p>
+                    …most of programming is an attempt to compensate for the strictly limited size of our skulls.
+                    The people who are the best programmers are the people who realize how small their brains are.
+                    They are humble. The people who are the worst at programming are the people who refuse to
+                    accept the fact that their brains aren’t equal to the task.
+                    Their egos keep them from being great programmers.
+                    The more you learn to compensate for your small brain, the better a programmer you’ll be.
+                    <span className="squiggle"> The more humble you are, the faster you’ll improve.</span>
+                  </p>
+                  <p>--From Steve McConnell's <em>Code Complete</em>.</p>
+                </>
+              }
+            />
+            <Route path="*" element={<h2>404 Page</h2>} />
+          </Routes>
+        </main>
+        <footer>
+          <p>Copyright © {new Date().getFullYear()}</p>
+        </footer>
+      </Container>
     </>
   );
 }
