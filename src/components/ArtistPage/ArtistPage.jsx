45a0c696102c25f6b86a178eefd0f76670eabbb9
Alexander Ruziska <ziskapants@gmail.com>
2025-04-06 15:06:27 -0500

    made some styling and added modal

--- src/components/ArtistPage/ArtistPage.jsx
+++ src/components/ArtistPage/ArtistPage.jsx
@@ -2,15 +2,14 @@ import React, { useState, useEffect } from "react";
 import useStore from "../../zustand/store";
 import { useParams, useNavigate } from "react-router-dom";
 import { Cloudinary } from "@cloudinary/url-gen/index";
-// import { AdvancedImage } from "@cloudinary/react";
 import { fill } from "@cloudinary/url-gen/actions/resize";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Card from "react-bootstrap/Card";
+import Modal from "react-bootstrap/Modal"; // Import Modal from react-bootstrap
 import "./ArtistPage.css";
 import UploadArtistProfileWidget from "../UploadArtistProfileWidget/UploadArtistProfileWidget";
 import Nav from 'react-bootstrap/Nav';
-
 
 function ArtistPage() {
   const { artistId } = useParams();
@@ -20,12 +19,13 @@ function ArtistPage() {
   const updateArtist = useStore((state) => state.updateArtist);
   const user = useStore((state) => state.user);
   
-
   const [isEditing, setIsEditing] = useState(false);
   const [editedArtist, setEditedArtist] = useState({});
   const [profilePhoto, setProfilePhoto] = useState(null);
 
-//UPDATE THE GALLERY MAPPING
+  // Modal state for full-size image display
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState("");
 
   useEffect(() => {
     fetchArtist(artistId);
@@ -33,38 +33,13 @@ function ArtistPage() {
     console.log('Artist obj in useEffect:', artistOBJ)
   }, [artistId]);
 
-  // useEffect(() => {
-  //   if (artistOBJ) {
-  //     setEditedArtist(artistOBJ);
-  //     // setProfilePhoto(artistOBJ.profile_pic);
-  //   }
-  // }, [artistOBJ]);
-
-//----CLOUDINARY INFO----//
-
-    // Create a Cloudinary instance and set your cloud name.
-    const cld = new Cloudinary({
-        cloud: {
-          cloudName: 'dwqjkxlqe'
-
-        }
-      });
-  
-        // Instantiate a CloudinaryImage object for the image with the public ID, 'docs/models'.
-    const myImage = cld.image('docs/result.info.public_id'); 
-  
-    // Resize to 250 x 250 pixels using the 'fill' crop mode.
-    myImage.resize(fill().width(150).height(150));  
-
-//----------------------//
-
   const handleChange = (e) => {
     const { name, value } = e.target;
-  setEditedArtist((prev) => ({ ...prev, [name]: value }));
+    setEditedArtist((prev) => ({ ...prev, [name]: value }));
   };
 
   const handleSave = () => {
-const myNewArtist = {...editedArtist, profile_pic: profilePhoto}
+    const myNewArtist = { ...editedArtist, profile_pic: profilePhoto };
     updateArtist(artistId, myNewArtist);
     setIsEditing(false);
   };
@@ -73,169 +48,210 @@ const myNewArtist = {...editedArtist, profile_pic: profilePhoto}
     navigate(`/artists/${artistOBJ.id}/ideas`);
   };
 
+  // Image click handler for modal
+  const handleImageClick = (imageUrl) => {
+    setSelectedImage(imageUrl);
+    setShowModal(true);
+  };
 
+  const handleCloseModal = () => setShowModal(false);
 
-        //navigation to add photos to an artist's profile
-        const newPhotoNav= () => {
-          navigate(`/photos`);
-      }
+  const newPhotoNav = () => {
+    navigate(`/photos`);
+  };
   
+
   const isMember = artistOBJ?.is_member;
 
   return artistOBJ ? (
     <div id="artistPage" className="mt-4">
       {isEditing ? (
         <Form>
-      <Form.Group className="mb-3">
-        <Form.Label>Artist Name</Form.Label>
-        <Form.Control
-          type="text"
-          name="name"
-          value={editedArtist.name || ""}
-          onChange={handleChange}
-          placeholder="Artist Name"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Headline Description</Form.Label>
-        <Form.Control
-          as="textarea"
-          name="headline_description"
-          value={editedArtist.headline_description || ""}
-          onChange={handleChange}
-          placeholder="Headline Description"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        {isEditing && (<UploadArtistProfileWidget setProfilePhoto={setProfilePhoto}/>)}
-        {editedArtist.profile_pic && (
-          <img src={editedArtist.profile_pic} alt="Preview" style={{ width: 200, height: "auto" }} />
-        )}
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>LinkedIn</Form.Label>
-        <Form.Control
-          type="text"
-          name="linkedin"
-          value={editedArtist.linkedin || ""}
-          onChange={handleChange}
-          placeholder="LinkedIn"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Facebook</Form.Label>
-        <Form.Control
-          type="text"
-          name="facebook"
-          value={editedArtist.facebook || ""}
-          onChange={handleChange}
-          placeholder="Facebook"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Instagram</Form.Label>
-        <Form.Control
-          type="text"
-          name="insta"
-          value={editedArtist.insta || ""}
-          onChange={handleChange}
-          placeholder="Instagram"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Website</Form.Label>
-        <Form.Control
-          type="text"
-          name="website"
-          value={editedArtist.website || ""}
-          onChange={handleChange}
-          placeholder="Website"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Bio</Form.Label>
-        <Form.Control
-          as="textarea"
-          name="bio"
-          value={editedArtist.bio || ""}
-          onChange={handleChange}
-          placeholder="Bio"
-        />
-      </Form.Group>
-      <Form.Group className="mb-3">
-        <Form.Label>Phone</Form.Label>
-        <Form.Control
-          type="text"
-          name="phone"
-          value={editedArtist.phone || ""}
-          onChange={handleChange}
-          placeholder="Phone"
-        />
-      </Form.Group>
-      <div className="mt-3">
-        <Button variant="success" onClick={handleSave}>Save</Button>
-        <Button variant="secondary" onClick={() => setIsEditing(false)} className="ms-2">Cancel</Button>
-      </div>
-    </Form>
-  ) : (
-    <>
-      <Card className="mb-3">
-        <Card.Header>{artistOBJ.name}</Card.Header>
-        <Card.Body>
-          <p>{artistOBJ.headline_description}</p>
-          <h4>Projects</h4>
-          {/* Soundcloud */}
-          <div>
-            {artistOBJ.soundcloud_id && (
-              <iframe width="100%" height="465" sallow="autoplay"
-                src={`https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/${artistOBJ.soundcloud_id}&amp;`} />
+          <Form.Group className="mb-3">
+            <Form.Label>Artist Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="name"
+              value={editedArtist.name || ""}
+              onChange={handleChange}
+              placeholder="Artist Name"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Headline Description</Form.Label>
+            <Form.Control
+              as="textarea"
+              name="headline_description"
+              value={editedArtist.headline_description || ""}
+              onChange={handleChange}
+              placeholder="Headline Description"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            {isEditing && (<UploadArtistProfileWidget setProfilePhoto={setProfilePhoto}/>)}
+            {editedArtist.profile_pic && (
+              <img src={editedArtist.profile_pic} alt="Preview" style={{ width: 200, height: "auto" }} />
             )}
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>LinkedIn</Form.Label>
+            <Form.Control
+              type="text"
+              name="linkedin"
+              value={editedArtist.linkedin || ""}
+              onChange={handleChange}
+              placeholder="LinkedIn"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Facebook</Form.Label>
+            <Form.Control
+              type="text"
+              name="facebook"
+              value={editedArtist.facebook || ""}
+              onChange={handleChange}
+              placeholder="Facebook"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Instagram</Form.Label>
+            <Form.Control
+              type="text"
+              name="insta"
+              value={editedArtist.insta || ""}
+              onChange={handleChange}
+              placeholder="Instagram"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Website</Form.Label>
+            <Form.Control
+              type="text"
+              name="website"
+              value={editedArtist.website || ""}
+              onChange={handleChange}
+              placeholder="Website"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Bio</Form.Label>
+            <Form.Control
+              as="textarea"
+              name="bio"
+              value={editedArtist.bio || ""}
+              onChange={handleChange}
+              placeholder="Bio"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+              type="text"
+              name="phone"
+              value={editedArtist.phone || ""}
+              onChange={handleChange}
+              placeholder="Phone"
+            />
+          </Form.Group>
+          <div className="mt-3">
+            <Button variant="success" onClick={handleSave}>Save</Button>
+            <Button variant="secondary" onClick={() => setIsEditing(false)} className="ms-2">Cancel</Button>
           </div>
-          {/* Artist Photos */}
-          {artistOBJ.photos && artistOBJ.photos.length > 0 && (
-            <div>
-              {artistOBJ?.photos?.map((photo) => (
-                <div key={photo.id}>
-                  <img src={photo.image_url} alt={photo.title} />
-                  <p>Title: {photo.title}</p>
-                  <p>Description: {photo.description}</p>
-                </div>
-              ))}
-            </div>
-          )}
-          {artistOBJ.profile_pic && (
-            <img src={artistOBJ.profile_pic} alt="Artist" className="rounded-3" style={{ width: 200, height: "auto" }} />
-          )}          
-          <p>{artistOBJ.bio}</p>
-          <h5>Links:</h5>
-          <p>Personal website:</p>
-          <p>{artistOBJ.website}</p>
-        {/* if they have  */}
-          <p>{artistOBJ.spotify_id}</p>
-          <p>LinkedIn:</p>
-          <p>{artistOBJ.linkedin}</p>
-          <p>Facebook:</p>
-          <p>{artistOBJ.facebook}</p>
-          <p>Instagram:</p>
-          <p>{artistOBJ.insta}</p>
-          <p>{artistOBJ.phone}</p>
-          <Button className="idea-button" onClick={ideaButton}>Artist Ideas</Button>
-          {isMember && (
-            <Button variant="secondary" onClick={() => setIsEditing(true)} className="ms-2">Edit profile</Button>
-          )}
-                      <div>
+        </Form>
+      ) : (
+        <>
+          <Card className="mb-3">
+            <Card.Header>{artistOBJ.name}</Card.Header>
+            <Card.Body>
+              <p>{artistOBJ.headline_description}</p>
+              <h4>Projects</h4>
+              {/* Soundcloud */}
+              <div>
+                {artistOBJ.soundcloud_id && (
+                  <iframe width="100%" height="465" sallow="autoplay"
+                    src={`https://w.soundcloud.com/player/?url=https%3A//soundcloud.com/${artistOBJ.soundcloud_id}&amp;`} />
+                )}
+              </div>
+              {/* Artist Portfolio Layout */}
+              {artistOBJ.photos && artistOBJ.photos.length > 0 && (
+                <>
+                  {/* First image large and full-width */}
+                  <div className="artist-feature-image" onClick={() => handleImageClick(artistOBJ.photos[0].image_url)}>
+                    <img src={artistOBJ.photos[0].image_url} alt={artistOBJ.photos[0].title} />
+                    <p className="mt-2"><strong>{artistOBJ.photos[0].title}</strong></p>
+                    <p>{artistOBJ.photos[0].description}</p>
+                  </div>
+
+                  {/* 2-column layout for remaining images */}
+                  <div className="artist-photo-grid">
+                    {artistOBJ.photos.slice(1).map((photo) => (
+                      <div key={photo.id} className="photo-tile" onClick={() => handleImageClick(photo.image_url)}>
+                        <img src={photo.image_url} alt={photo.title} />
+                        <p><strong>{photo.title}</strong></p>
+                        <p>{photo.description}</p>
+                      </div>
+                    ))}
+                  </div>
+                </>
+              )}
+              {artistOBJ.profile_pic && (
+                <img src={artistOBJ.profile_pic} alt="Artist" className="rounded-3" style={{ width: 200, height: "auto" }} />
+              )}
+              <p>{artistOBJ.bio}</p>
+              <h5>Links:</h5>
+              <p>Personal website:</p>
+              <p>{artistOBJ.website}</p>
+              <p>{artistOBJ.spotify_id}</p>
+              <p>LinkedIn:</p>
+              <p>{artistOBJ.linkedin}</p>
+              <p>Facebook:</p>
+              <p>{artistOBJ.facebook}</p>
+              <p>Instagram:</p>
+              <p>{artistOBJ.insta}</p>
+              <p>{artistOBJ.phone}</p>
+              <Button className="idea-button" onClick={ideaButton}>Artist Ideas</Button>
+              {isMember && (
+                <Button variant="secondary" onClick={() => setIsEditing(true)} className="ms-2">Edit profile</Button>
+              )}
+              <div>
                 {user.artist_id && <button onClick={newPhotoNav} >Add art to profile</button>}
-            </div>
-        </Card.Body>
-      </Card>
-    </>
-  )}
-</div>
-) : (
-<p>Loading...</p>
-);
+              </div>
+            </Card.Body>
+          </Card>
+        </>
+      )}
+
+      {/* Modal for full-size image */}
+      <Modal
+  show={showModal}
+  onHide={handleCloseModal}
+  centered
+  size="lg"
+  className="custom-modal-size"
+>
+  <Modal.Header closeButton={false} className="custom-modal-header">
+    {/* "X" button to close the modal */}
+    <button 
+      type="button" 
+      className="close-button" 
+      onClick={handleCloseModal}
+    >
+      ×
+    </button>
+  </Modal.Header>
+  <Modal.Body>
+    {/* Ensure the image takes up as much space as possible */}
+    <img src={selectedImage} alt="Full size" className="img-fluid modal-image" />
+  </Modal.Body>
+</Modal>
+
+
+
+
+    </div>
+  ) : (
+    <p>Loading...</p>
+  );
 }
-
-
 
 export default ArtistPage;
